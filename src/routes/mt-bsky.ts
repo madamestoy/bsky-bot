68ed1d654e7cd9e7ebd48081e483ecfe192ceee8
madamestoy <toy@madamestoy.com>
2024-09-21 13:25:04 +0000

    chore: added logging

--- src/routes/mt-bsky.ts
+++ src/routes/mt-bsky.ts
@@ -14,19 +14,24 @@ BskyRoutes.get('/mt/daily-post', async (req, res) => {
   const today = new Date();
   const monthName = format(today, 'LLLL').toLowerCase();
   const day = getDate(today);
+  console.log(`DAILY POST: Get Post for ${monthName}/${day}`);
   const daysPost = await checkHasPostedDailyPost(monthName, day);
+  console.log(`DAILY POST: Days Post`, JSON.stringify(daysPost));
   let post;
   if (daysPost) {
+    console.log(`DAILY POST: Post It!`);
     post = `For day ${daysPost.day} - ${daysPost.post}`;
     res
       .status(200)
       .json({ message: "Already sent toda's message!", today, post });
   } else {
+    console.log(`DAILY POST: Already Posted!`);
     post = await getPostForDate(today);
     await createPost(post, today);
     await trackTodaysPost(monthName, day, post);
     res.status(200).json({ message: "Today's message posted!", today, post });
   }
+  console.log(`DAILY POST: /////////////////////`);
 });
 
 BskyRoutes.get('/mt/history', async (req, res) => {
